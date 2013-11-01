d8cc79cab2e1835161aacc2ecfd17c7684eee048
Grant Rodgers <grantr@gmail.com>
2013-11-01 13:26:22 -0700

    add some ruby for testing syntax

---
+++ test.rb
@@ -0,0 +1,37 @@
+require "thing.rb"
+HELLO true false 1 2 "this is a string end #{ method_call; def thing(arg1, arg2) end; 123 + true}"; /regexp.*/
+
+module Thing
+  extend self
+end
+
+class Thing
+  thing {
+  }
+
+  def initialize
+    super
+  end
+end
+
+a.hi = Proc.new()
+
+a = "blah"
+:symbol
+
+def thing(arg1, arg2)
+  thing(arg1, arg2)
+
+end
+
+thing(arg1, arg2) do
+end
+
+thing {
+}
+
+# This is a comment
+
+=begin
+This is a block comment
+=end
